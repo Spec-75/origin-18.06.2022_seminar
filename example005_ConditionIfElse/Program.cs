6a2518798709067bac929b5acec64076e20f85fd
Spec-75 <Spec_75@mail.ru>
2022-06-23 22:04:36 +0300

    example005 ConditionIfElse

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+        
+    
